f5445cd39b05f9c69ca1fb3d5053563856dd5f63
EduardoJAVA <eduardo.castillo@makingpit.com>
2017-10-07 11:53:46 -0500

    Mi primer commit

---
+++ src/main/java/operaciones/Operaciones.java
@@ -0,0 +1,13 @@
+package operaciones;
+
+public class Operaciones {
+    
+    public int sumar(int x, int y){
+        return x + y;
+    }
+    
+    public int restar(int x, int y){
+        return x - y;
+    }
+    
+}
